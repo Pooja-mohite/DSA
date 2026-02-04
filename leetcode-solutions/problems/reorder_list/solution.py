9b5a012431bd17f4f6b76f76f6fb25c73c42ea3b
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-02-04 17:02:29 +0000

    Sync LeetCode submission - Reorder List (python)

---
+++ leetcode-solutions/problems/reorder_list/solution.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reorderList(self, head):
+        slow, fast = head, head
+        while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+        prev, curr = None, slow.next
+        slow.next = None
+        while curr:
+                    next_temp = curr.next
+                    curr.next = prev
+                    prev = curr
+                    curr = next_temp
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
+
+       
+        
