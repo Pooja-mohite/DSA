071a28c4ab50c6f994902f6f1422a5080385a543
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-04-21 00:13:30 +0530

    Time: 31 ms (28.82%) | Memory: 13.3 MB (25.55%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         #brute force
-        n = len(s)
+        """n = len(s)
         max_length = 0
         for i in range(n):
             seen = set()
@@ -10,5 +10,17 @@ class Solution(object):
                     break
                 seen.add(s[j])
                 max_length = max(max_length, j-i + 1)
+        return max_length"""
+
+        # sliding window
+        char_set = set()
+        start = 0
+        max_length = 0
+        for end in range(len(s)):
+            while s[end] in char_set:
+                char_set.remove(s[start])
+                start = start + 1
+            char_set.add(s[end])
+            max_length = max(max_length, end-start + 1)
         return max_length
             
