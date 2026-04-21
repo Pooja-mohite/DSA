05fc0179e01631e8168ed6518ff31dd31c2440f8
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-04-21 07:05:25 +0530

    Time: 3 ms (73.69%) | Memory: 12.6 MB (41.28%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,10 +1,25 @@
 class Solution(object):
     def isValid(self, s):
         #brute force
-        while "()" in s or "[]" in s or "{}" in s:
+        """while "()" in s or "[]" in s or "{}" in s:
             s = s.replace("()", "")
             s = s.replace("[]", "")
             s = s.replace("{}", "")
-        return s == ""
+        return s == "" """
+        
+        # stack
+        stack = []
+        for para in s:
+            if para in "([{":
+                stack.append(para)
+            else:
+                if not stack:
+                    return False
+                top = stack.pop()
+                if (para == ")" and top != "(") or \
+                   (para == "]" and top != "[") or \
+                   (para == '}' and top != "{"):
+                   return False
+        return len(stack) == 0
         
         
