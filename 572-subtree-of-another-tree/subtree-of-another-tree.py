205af7be63b3ffa74219fba2710b5cef95caeb85
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-04-22 11:50:36 +0530

    Time: 89 ms (15.30%) | Memory: 13.6 MB (39.35%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -6,20 +6,20 @@
 #         self.right = right
 class Solution(object):
     def isSubtree(self, root, subRoot):
-        def isSametree(p,q):
-            if not p and not q:
-                return True
-            if not p or not q:
-                return False
-            if p.val != q.val:
-                return False
-            return isSametree(p.left,q.left) and isSametree(p.right,q.right)
-        if not root:
+        if root is None:
             return False
-        if isSametree(root,subRoot):
+        if self.isSametree(root, subRoot):
             return True
-        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
-        
+        left = self.isSubtree(root.left, subRoot)
+        right = self.isSubtree(root.right, subRoot)
+        return left or right
+    def isSametree(self,p,q):
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSametree(p.left, q.left) and self.isSametree(p.right, q.right)
 
-    
-       
+           
