e618bcd3ecc25150edabfe91ff74a185a68f0256
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-04-25 11:13:26 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (58.88%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def climbStairs(self, n):
+        #brute force
+        """
+        if n == 0 or n == 1:
+            return 1
+
+        return self.climbStairs(n-1) + self.climbStairs(n-2)
+        """
+
+        #DP
+        #dp array
+        dp = [0] * (n+1)
+
+        dp[0] = 1
+        dp[1] = 1
+
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
+
+
+        
+        
+        
