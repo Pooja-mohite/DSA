ec1d85265c35aa4d69375bc4cf83d99ef00f08da
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-04-27 22:39:55 +0530

    Time: 1 ms (33.52%) | Memory: 12.7 MB (5.73%) - LeetSync

---
+++ 62-unique-paths/unique-paths.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+
+        # brute forec:
+        """
+        def dfs(i,j):
+    
+            if i >=m or j>=n:
+                return 0
+            if i == m-1 and j == n-1:
+                return 1
+            right = dfs(i, j+1)
+            down = dfs(i+1, j)
+            return right + down
+        return dfs(0,0)
+        """
+        # optimized
+        dp = [[-1]*n for _ in range(m)]
+        def dfs(i, j):
+            if i >= m or j >= n:
+                return 0
+            if i == m-1 and j == n-1:
+                return 1
+            
+            if dp[i][j] != -1:
+                return dp[i][j]
+                
+            right = dfs(i, j+1)
+            down = dfs(i+1, j)
+
+            dp[i][j] = right + down
+
+            return dp[i][j]
+        return dfs(0,0)
+        
+
+
+        
+        
+        
