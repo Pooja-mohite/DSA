a495baa8deabae12e89c49df2b58a00e5bc98710
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-05-01 16:41:00 +0530

    Time: 17 ms (22.04%) | Memory: 13.4 MB (37.59%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
+        """
         # brute force:
         m = len(matrix)
         n = len(matrix[0])
@@ -13,7 +14,22 @@ class Solution(object):
                 if matrix[i][j] == 0:
                     zeros.append((i,j))
         for (i,j) in zeros:
-            for row in range(m):
-                matrix[row][j] = 0
             for col in range(n):
                 matrix[i][col] = 0
+            for row in range(m):
+                matrix[row][j] = 0
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        rows = [0] *m
+        cols = [0] * n
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    rows[i] = 1
+                    cols[j] = 1
+        for i in range(m):
+            for j in range(n):
+                if rows[i] == 1 or cols[j] ==1:
+                    matrix[i][j] = 0
+
