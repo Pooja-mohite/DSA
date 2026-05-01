8a1929607db728a83a5fdbf0f9e0238804e2f1c5
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-05-01 17:36:11 +0530

    Time: 8 ms (75.55%) | Memory: 13.5 MB (29.12%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -19,6 +19,7 @@ class Solution(object):
             for row in range(m):
                 matrix[row][j] = 0
         """
+        """
         m = len(matrix)
         n = len(matrix[0])
         rows = [0] *m
@@ -32,4 +33,37 @@ class Solution(object):
             for j in range(n):
                 if rows[i] == 1 or cols[j] ==1:
                     matrix[i][j] = 0
+        """
+
+        #optimized
+        m = len(matrix)
+        n = len(matrix[0])
+        firstrowzero = False
+        firstcolzero = False
+
+        #check first row
+        for  j in range(n):
+            if matrix[0][j] ==0:
+                firstrowzero = True
+        for i in range(m):
+            if matrix[i][0] == 0:
+                firstcolzero = True
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][0]==0 or matrix[0][j] ==0:
+                    matrix[i][j] =0
+
+        if firstrowzero:
+            for j in range(n):
+                matrix[0][j] = 0
+
+        if firstcolzero:
+            for i in range(m):
+                matrix[i][0] = 0
 
