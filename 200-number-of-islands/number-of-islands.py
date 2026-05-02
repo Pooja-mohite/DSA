3485dc90dde85bc161c0cdc3bda73a6ee4585ccf
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-05-02 20:03:00 +0530

    Time: 229 ms (53.05%) | Memory: 27.9 MB (49.03%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,30 +2,23 @@ class Solution(object):
     def numIslands(self, grid):
         rows = len(grid)
         cols = len(grid[0])
-
-        def dfs(i, j):
-            #boundary check
-            if i < 0 or j < 0 or i >= rows or j >= cols:
-                return
-
+        def dfs(i,j):
+            if i<0 or j<0 or i>=rows or j>=cols:
+                return 
             if grid[i][j] == "0":
                 return
-
-            #mark visited
-            grid[i][j] = "0"
-
-            #visit all 4 directions
-            dfs(i+1, j)
-            dfs(i-1, j)
+            grid[i][j] = '0'
             dfs(i, j+1)
             dfs(i, j-1)
-
-        count = 0
-
+            dfs(i-1, j)
+            dfs(i+1,j)
+        count=0
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == "1":
-                    dfs(i, j)
-                    count = count + 1
+                    dfs(i,j)
+                    count = count +1
         return count
-        
+            
+
+            
