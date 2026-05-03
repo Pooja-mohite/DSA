1c04ef5300f3dea18788ea6318bc52502ee11698
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-05-03 12:57:41 +0530

    Time: 4 ms (32.52%) | Memory: 14.5 MB (5.08%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,29 +1,16 @@
 class Solution(object):
     def insert(self, intervals, newInterval):
-        #brute force=
-
-        #add new interval
         intervals.append(newInterval)
-
-        #sort
         intervals.sort(key=lambda x: x[0])
-
         result = []
-
-        #add first interval
         result.append(intervals[0])
         for i in range(1, len(intervals)):
             current = intervals[i]
-            last = result[-1]
-
-            #if no overlap
-            if current[0] > last[1]:
+            prev = result[-1]
+            if current[0] > prev[1]:
                 result.append(current)
-            # ifoverlap
             else:
-                last[0] = min(last[0], current[0])
-                last[1] = max(last[1], current[1])
+                prev[0] = min(current[0], prev[0])
+                prev[1] = max(current[1], prev[1])
         return result
-
-        
         
