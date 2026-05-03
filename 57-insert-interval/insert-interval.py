3c0140c6ff025fc94757149761a1c0cedb77d582
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-05-03 13:42:46 +0530

    Time: 4 ms (32.52%) | Memory: 14.3 MB (5.08%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,5 +1,7 @@
 class Solution(object):
     def insert(self, intervals, newInterval):
+        '''
+        #brute force
         intervals.append(newInterval)
         intervals.sort(key=lambda x: x[0])
         result = []
@@ -13,4 +15,20 @@ class Solution(object):
                 prev[0] = min(current[0], prev[0])
                 prev[1] = max(current[1], prev[1])
         return result
-        
+        '''
+        #optimized
+        n = len(intervals)
+        result = []
+        i = 0
+        while i < n and intervals[i][1] < newInterval[0]:
+            result.append(intervals[i])
+            i = i+1
+        while i<n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(intervals[i][0], newInterval[0])
+            newInterval[1] = max(intervals[i][1], newInterval[1])
+            i = i+1
+        result.append(newInterval)
+        while i<n:
+            result.append(intervals[i])
+            i = i+1
+        return result
