3c5a2efc4e6f4aeb3c9fc5bcbd201bdcdd9f117f
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-05-03 16:41:58 +0530

    Time: 7 ms (97.28%) | Memory: 15.9 MB (90.53%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,29 +1,50 @@
 class Solution(object):
     def merge(self, intervals):
+        '''
+        #brute force
+        result = intervals[:]
+        while True:
+            n = len(result)
+            merged = False
+            for i in range(n):
+                for j in range(i+1,n):
+                    a = result[i]
+                    b = result[j]
+                    if a[1] >= b[0] and b[1] >= a[0]:
+                        newstart = min(a[0],b[0])
+                        newend= max(a[1], b[1])
 
-        # Brute Force=
-      
-        intervals.sort(key=lambda x: x[0])
-
-        result = []
-
-        for interval in intervals:
-            if len(result) == 0:
-                result.append(interval)
-
-            else:
-                last = result[-1]
-
-                #  check overlap
-                if interval[0] <= last[1]:
-                    #merge
-                    last[0] = min(last[0],interval[0])
-                    last[1] = max(last[1],interval[1])
-                
-                else:
-                    result.append(interval)
+                        result.pop(j)
+                        result.pop(i)
+                        result.append([newstart, newend])
+                        merged = True
+                        break
+                if merged:
+                        break
+            if not merged:
+                    break
         return result
+        '''
 
+        #optimized
+        intervals.sort(key=lambda x: x[0])
+        merged = []
+        for interval in intervals:
+            if len(merged) == 0:
+                merged.append(interval)
+            else:
+                prev_interval = merged[-1]
+
+                if interval[0] <= prev_interval[1]:
+                    prev_interval[0] = min(prev_interval[0], interval[0])
+                    prev_interval[1] = max(prev_interval[1], interval[1])
+                else:
+                    merged.append(interval)
+        return merged
+
+                
+    
+        
 
                     
 
