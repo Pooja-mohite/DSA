c09f8cf893e1d81bae77388299797d35bbd4fa8d
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-05-04 16:32:37 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (6.48%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -7,38 +7,15 @@
 from collections import deque
 class Solution(object):
     def levelOrder(self, root):
-        #brute force(dfs- recursive)
-        """
         result = []
         def dfs(node, level):
             if node is None:
-                return
+                return 
             if len(result) == level:
                 result.append([])
             result[level].append(node.val)
             dfs(node.left, level+1)
             dfs(node.right, level+1)
-        dfs(root, 0)
+        dfs(root,0)
         return result
-        """
-        # optimized(BFS - QUEUE)
-        result = []
-        if root is None:
-            return result
-        queue = deque()
-        queue.append(root)
-        while len(queue) > 0:
-            level = []
-            size = len(queue)
-            i = 0
-            while i<size:
-                node = queue.popleft()
-                level.append(node.val)
-                if node.left is not None:
-                    queue.append(node.left)
-                if node.right is not None:
-                    queue.append(node.right)
-                i = i+1
-            result.append(level)
-        return result
-
+       
