37048d65a071e593c5e7c3507de8c79d97e2f953
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-05-04 19:09:37 +0530

    Time: 62 ms (16.74%) | Memory: 20.4 MB (85.30%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -2,42 +2,30 @@
 
 class Solution(object):
     def lowestCommonAncestor(self, root, p, q):
-        """
-        pathofp = []
-        pathofq = []
-        self.findPath(root, p, pathofp)
-        self.findPath(root, q, pathofq)
+        p_path = []
+        q_path = []
+        self.findpath(root, p_path, p)
+        self.findpath(root, q_path, q)
         i = 0
-        lca = None
-        while i< len(pathofp) and i < len(pathofq):
-            if pathofp[i] == pathofq[i]:
-                lca = pathofp[i]
+        lowestrancestor = None
+        while i < len(p_path) and i < len(q_path):
+            if p_path[i] == q_path[i]:
+                lowestancestor = p_path[i]
             else:
                 break
             i = i+1
-        return lca
-    def findPath(self, root, target, path):
-        #brute force
+        return lowestancestor
+                                                                                             
+    def findpath(self, root, path, target):
         if root is None:
             return False
         path.append(root)
         if root.val == target.val:
             return True
-        if self.findPath(root.left, target, path):
+        if self.findpath(root.left, path, target):
             return True
-        if self.findPath(root.right, target, path):
+        if self.findpath(root.right, path, target):
             return True
         path.pop()
         return False
-        """
-        while root:
-            if p.val < root.val and q.val < root.val:
-                root = root.left
-            elif p.val > root.val and q.val > root.val:
-                root = root.right
-            else:
-                return root
-
-
-
-     
+        
