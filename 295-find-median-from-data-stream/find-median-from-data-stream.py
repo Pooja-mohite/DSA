51c8e61034334a78840ae077d62d9b744c9d8815
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-05-07 18:10:18 +0530

    Time: 1062 ms (30.81%) | Memory: 35.9 MB (18.67%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,52 +1,45 @@
 class MedianFinder(object):
-    #brute force
     """
+    #brute force
+    
     def __init__(self):
-        self.arr = []
+        self.Mlist = []
         
 
     def addNum(self, num):
-        self.arr.append(num)
-        self.arr.sort()
-       
+        self.Mlist.append(num)
+        self.Mlist.sort()
         
-
     def findMedian(self):
-        n = len(self.arr)
-        
+        n = len(self.Mlist)
         if n % 2 == 1:
-            return self.arr[n // 2]
+            return self.Mlist[n//2]
         else:
-            mid1 = self.arr[n //2]
-            mid2 = self.arr[n//2-1]
-            return (mid1 + mid2) / 2.0
+            mid1 = self.Mlist[n//2]
+            mid2 = self.Mlist[n//2-1]
+            mid = (mid1+mid2)/2.0
+            return mid
             """
+    #optimized
     def __init__(self):
         self.small = []
         self.large = []
+
     def addNum(self, num):
         heapq.heappush(self.small, -num)
-
-        val = -heapq.heappop(self.small)
-        heapq.heappush(self.large, val)
-
+        value = -heapq.heappop(self.small)
+        heapq.heappush(self.large,value)
         if len(self.large) > len(self.small):
-            val = heapq.heappop(self.large)
-            heapq.heappush(self.small, -val)
+            value = heapq.heappop(self.large)
+            heapq.heappush(self.small, -value)
 
     def findMedian(self):
-
-        #odd
         if len(self.small) > len(self.large):
             return -self.small[0]
-        
-        #even
         return (-self.small[0] + self.large[0]) / 2.0
-        
-        
 
 
-# Your MedianFinder object will be instantiated and called as such:
-# obj = MedianFinder()
-# obj.addNum(num)
-# param_2 = obj.findMedian()
+
+    
+
+        
