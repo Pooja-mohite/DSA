4f026106722ce794d120d0136df7251dfc7b643f
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-05-11 21:37:39 +0530

    Time: 120 ms (33.20%) | Memory: 17.9 MB (64.40%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,5 +1,8 @@
+import heapq
 class Solution(object):
     def kClosest(self, points, k):
+        #  brute fore:
+        """
         arr = []
         for point in points:
             x = point[0]
@@ -11,5 +14,21 @@ class Solution(object):
         for i in range(k):
             result.append(arr[i][1])
         return result
+        """
+        # optimized (heap)
+        heap = []
+        for point in points:
+            x = point[0]
+            y = point[1]
+            distance = x * x + y * y
+            heapq.heappush(heap, (distance, point))
+        result = []
+        for i in range(k):
+            distance,point = heapq.heappop(heap)
+            result.append(point)
+        return result
+            
+                            
+
         
         
