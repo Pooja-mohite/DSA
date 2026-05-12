08f503fe12418aecbf37592e9a0364bb27fc77b0
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-05-12 18:59:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (21.70%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def permute(self, nums):
+        #brute force
+        n = len(nums)
+        result= []
+        current = []
+        visited = [False]*n
+        def backtrack():
+            if len(current) == n:
+                result.append(current[:])
+                return
+            for i in range(n):
+                if visited[i]:
+                    continue
+                visited[i] = True
+                current.append(nums[i])
+                backtrack()
+                current.pop()
+                visited[i] = False
+        backtrack()
+        return result
+
+        
+        
