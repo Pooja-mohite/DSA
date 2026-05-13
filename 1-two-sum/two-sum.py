72d5c73de67aeb82e3d41e145d5d398cf56ae09a
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-05-13 22:26:30 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (35.33%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,20 @@
 class Solution(object):
     def twoSum(self, nums, target):
+        '''
+        # brute force
         n = len(nums)
         for i in range(n):
             for j in range(i+1, n):
                 if nums[i] + nums[j] == target:
                     return [i,j]
+                    '''
+        #hashmap
+        hashmap = {}
+        for i in range(len(nums)):
+            required = target-nums[i]
+            if required in hashmap:
+                return [hashmap[required], i]
+            hashmap[nums[i]] = i
 
         
         
