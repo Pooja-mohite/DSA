352855cab1f60ce0bb73cb480dcda0cef1a41d32
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-05-13 23:07:19 +0530

    Time: 35 ms (58.47%) | Memory: 12.6 MB (88.12%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,5 +1,7 @@
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
+        """
+        #  brute force
         chars = list(magazine)
         for char in ransomNote:
             found = False
@@ -11,4 +13,19 @@ class Solution(object):
             if found == False:
                 return False
         return True
+        """
+        # hashmap
+        count = {}
+        for char in magazine:
+            count[char] = count.get(char, 0)+1
+        for char in ransomNote:
+            if char not in count:
+                return False
+            if count[char] == 0:
+                return False
+            count[char] = count[char]-1
+        return True
+
+
+        
         
