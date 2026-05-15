0a78e3942ede23f11ab7cebd2f937c911e57d2be
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-05-15 19:49:12 +0530

    Time: 610 ms (5.08%) | Memory: 13.6 MB (5.34%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,30 +1,21 @@
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
-        """
-        #  brute force
-        chars = list(magazine)
+        
+        character = list(magazine)
+        n = len(character)
         for char in ransomNote:
             found = False
-            for i in range(len(chars)):
-                if chars[i] == char:
+            for i in range(n):
+                if character[i] == char:
                     found = True
-                    chars[i] = "#"
+                    character[i] = "#"
                     break
             if found == False:
                 return False
         return True
-        """
-        # hashmap
-        count = {}
-        for char in magazine:
-            count[char] = count.get(char, 0)+1
-        for char in ransomNote:
-            if char not in count:
-                return False
-            if count[char] == 0:
-                return False
-            count[char] = count[char]-1
-        return True
+
+
+     
 
 
         
