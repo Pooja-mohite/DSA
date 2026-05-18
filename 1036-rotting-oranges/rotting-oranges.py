9ad5fa1e38b9b517090b1d83866f387d58bf4084
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-05-18 19:43:15 +0530

    Time: 12 ms (8.19%) | Memory: 12.3 MB (57.47%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,72 +1,42 @@
 class Solution(object):
     def orangesRotting(self, grid):
-        '''
-        #brute force
         rows = len(grid)
         cols = len(grid[0])
         minutes = 0
         while True:
-            new_rotten = []
+            newRotten = []
             for i in range(rows):
                 for j in range(cols):
                     if grid[i][j] == 2:
-                        if i-1 >= 0 and grid[i-1][j] == 1:
-                            new_rotten.append((i-1,j))
                         if i+1 < rows and grid[i+1][j] == 1:
-                            new_rotten.append((i+1,j))
-                        if j-1 >= 0 and grid[i][j-1] == 1:
-                            new_rotten.append((i, j-1))
+                            newRotten.append((i+1,j))
+                        if i-1 >= 0 and grid[i-1][j]== 1:
+                            newRotten.append((i-1, j))
                         if j+1 < cols and grid[i][j+1] == 1:
-                            new_rotten.append((i, j+1))
-            
-            if len(new_rotten) == 0:
+                            newRotten.append((i, j+1))
+                        if j-1 >= 0 and grid[i][j-1] == 1:
+                            newRotten.append((i, j-1))
+            if len(newRotten) == 0:
                 break
-            for x,y in new_rotten:
-                grid[x][y]=2
+            for x,y in newRotten:
+                grid[x][y] = 2
             minutes = minutes + 1
-        
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == 1:
                     return -1
         return minutes
-        '''
-        #BFS
-        rows = len(grid)
-        cols = len(grid[0])
-        queue = deque()
-        fresh = 0
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == 2:
-                    queue.append((i,j))
-                if grid[i][j] == 1:
-                    fresh = fresh+1
-        minutes = 0
-        while queue and fresh>0:
-            size = len(queue)
-            for k in range(size):
-                i,j = queue.popleft()
-                
-                if i-1 >= 0 and grid[i-1][j] == 1:
-                    grid[i-1][j] = 2
-                    queue.append((i-1, j))
-                    fresh = fresh -1
-                if i+1 < rows and grid[i+1][j] == 1:
-                    grid[i+1][j] = 2
-                    queue.append((i+1,j))
-                    fresh = fresh - 1
-                if j-1 >=0 and grid[i][j-1] == 1:
-                    grid[i][j-1] = 2
-                    queue.append((i, j-1))
-                    fresh = fresh - 1
-                if j+1 < cols and grid[i][j+1] == 1:
-                    grid[i][j+1] = 2
-                    queue.append((i, j+1))
-                    fresh = fresh -1
-            minutes = minutes+1
-        if fresh == 0:
-            return minutes
-        return -1
 
+
+
+
+
+
+
+
+
+                    
+
+
+       
         
