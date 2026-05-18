1efe61e8db49692644015500e08e2aa16757dbd3
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-05-18 19:57:48 +0530

    Time: 3 ms (88.61%) | Memory: 12.3 MB (57.47%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,5 +1,7 @@
+
 class Solution(object):
     def orangesRotting(self, grid):
+        '''
         rows = len(grid)
         cols = len(grid[0])
         minutes = 0
@@ -26,6 +28,60 @@ class Solution(object):
                 if grid[i][j] == 1:
                     return -1
         return minutes
+        '''
+        rows = len(grid)
+        cols = len(grid[0])
+        fresh = 0
+        queue = deque()
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 2:
+                    queue.append((i,j))
+                if grid[i][j] == 1:
+                    fresh = fresh + 1
+        minutes = 0
+        while queue and fresh>0:
+            size = len(queue)
+            for k in range(size):
+                i,j = queue.popleft()
+                if i-1 >= 0 and grid[i-1][j] == 1:
+                    grid[i-1][j]=2
+                    queue.append((i-1,j))
+                    fresh = fresh - 1
+                if i+1 < rows and grid[i+1][j] == 1:
+                    grid[i+1][j] = 2
+                    queue.append((i+1,j))
+                    fresh = fresh -1 
+                if j-1 >= 0 and grid[i][j-1]==1:
+                    grid[i][j-1] = 2
+                    queue.append((i,j-1))
+                    fresh = fresh - 1
+                if j+1 < cols and grid[i][j+1] == 1:
+                    grid[i][j+1] = 2
+                    queue.append((i, j+1))
+                    fresh = fresh -1
+            minutes = minutes+1
+        if fresh == 0:
+            return minutes
+        return -1
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
 
 
 
