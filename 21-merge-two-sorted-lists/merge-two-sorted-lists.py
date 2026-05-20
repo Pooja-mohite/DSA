9aa0cd1ae06b47682bdfc8df3c78f62fa8213be3
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-05-20 21:59:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (47.63%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,52 +5,28 @@
 #         self.next = next
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
+        values =[]
+        temp = list1
+        while temp:
+            values.append(temp.val)
+            temp = temp.next
+        temp = list2
+        while temp:
+            values.append(temp.val)
+            temp = temp.next
+        values.sort()
+        dummy = ListNode(0)
+        current = dummy
+        for value in values:
+            current.next = ListNode(value)
+            current = current.next
+        return dummy.next
 
-      #brute force
-      """
-      values = []
-      temp = list1
-      while temp:
-        values.append(temp.val)
-        temp = temp.next
-      temp = list2
-      while temp:
-        values.append(temp.val)
-        temp = temp.next
-      values.sort()
-      dummy = ListNode(0)
-      current = dummy
-      for value in values:
-        current.next = ListNode(value)
-        current = current.next
-      return dummy.next
-      """
 
-      # Linked list
-      if not list1:
-        return list2
-      if not list2:
-        return list1
-      if list1.val < list2.val:
-        head = list1
-        list1 = list1.next
-      else:
-        head = list2
-        list2 = list2.next
-      current = head
-      while list1 and list2:
-        if list1.val < list2.val:
-            current.next = list1
-            list1 = list1.next
-        else:
-            current.next = list2
-            list2 = list2.next
-        current = current.next
-      if list1:
-        current.next = list1
-      else:
-        current.next = list2
-      return head
-    
 
-        
+
+
+
+
+
+
