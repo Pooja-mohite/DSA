41e3d46050953607ca0dbabe0e41857d4bdb9503
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-05-21 20:59:50 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (36.85%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,9 +1,23 @@
 class Solution(object):
     def search(self, nums, target):
+        '''
+        #brute force
         n = len(nums)
         for i in range(n):
             if nums[i] == target:
                 return i
+        return -1
+        '''
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
         return -1
         
         
