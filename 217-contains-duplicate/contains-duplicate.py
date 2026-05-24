93fbae213471293cee53c4c26ee9e7582f352298
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-05-24 14:32:31 +0530

    Time: 22 ms (34.83%) | Memory: 25.9 MB (56.68%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,20 +1,25 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        #brute force
-        """n = len(nums)
+        '''
+        # brute force
+        n = len(nums)
         for i in range(n-1):
             for j in range(i+1, n):
-                if nums[i] == nums[j]:
+                if nums[i]== nums[j]:
                     return True
-            return False"""
-
-        # hahset
-        seen = set()
-        for i in nums:
-            if i in seen:
-                return True
-            seen.add(i)
         return False
+       '''
+       #  hashset
+        n = len(nums)
+        hashset = set()
+        for i in nums:
+            if i in hashset:
+                return True
+            hashset.add(i)
+        return False
+
+
+
 
         
 
