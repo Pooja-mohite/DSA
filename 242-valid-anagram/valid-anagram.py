7dfa9339005221fe931c712b950af675d29216b4
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-05-25 17:55:27 +0530

    Time: 27 ms (24.39%) | Memory: 13.4 MB (40.09%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,27 +1,12 @@
 class Solution(object):
     def isAnagram(self, s, t):
         #brute force
-        """ss = sorted(s)
-        st = sorted(t)
-        if ss == st:
+        sorts = sorted(s)
+        sortt = sorted(t)
+        if sorts == sortt:
             return True
-        return False"""
+        return False
 
-        #hashmap
-        if len(s)!= len(t):
-            return False
-        count = {}
-        for letter in s:
-            count[letter] = count.get(letter,0) + 1
-        for letter in t:
-            if letter not in count:
-                return False
-            count[letter] -= 1
-            if count[letter] < 0:
-                return False
-        return True
         
-            
-       
-        
+
         
