6fd104d149b73121628b7ddc0199d3ef7cbebce1
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-06-02 19:46:13 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (5.18%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,9 +1,7 @@
 class Solution(object):
     def insert(self, intervals, newInterval):
-        '''
-        #brute force
         intervals.append(newInterval)
-        intervals.sort(key=lambda x: x[0])
+        intervals.sort(key = lambda x : x[0])
         result = []
         result.append(intervals[0])
         for i in range(1, len(intervals)):
@@ -12,23 +10,10 @@ class Solution(object):
             if current[0] > prev[1]:
                 result.append(current)
             else:
-                prev[0] = min(current[0], prev[0])
-                prev[1] = max(current[1], prev[1])
+               prev[0] =  min(current[0], prev[0])
+               prev[1] = max(current[1], prev[1])
         return result
-        '''
-        #optimized
-        n = len(intervals)
-        result = []
-        i = 0
-        while i < n and intervals[i][1] < newInterval[0]:
-            result.append(intervals[i])
-            i = i+1
-        while i<n and intervals[i][0] <= newInterval[1]:
-            newInterval[0] = min(intervals[i][0], newInterval[0])
-            newInterval[1] = max(intervals[i][1], newInterval[1])
-            i = i+1
-        result.append(newInterval)
-        while i<n:
-            result.append(intervals[i])
-            i = i+1
-        return result
+
+
+
+        
