731269b5e8ecd2dd62a4353ec7547fa5b02c9c08
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-06-08 21:58:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (19.56%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -12,6 +12,8 @@ class Solution(object):
                 if nums[j] > nums[j+1]:
                     nums[j], nums[j+1] = nums[j+1], nums[j]
                     """
+        # two pointers  
+        '''           
         n = len(nums)            
         left = 0
         right = n-1
@@ -23,6 +25,22 @@ class Solution(object):
             if nums[i] == 2:
                 nums[i], nums[right] = nums[right], nums[i]
                 right = right - 1
+                '''
+        # 3 pointers(dutch national flag)
+        n = len(nums)
+        low = 0
+        mid = 0
+        high = n-1
+        while mid<= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low = low + 1
+                mid = mid+1
+            elif nums[mid] == 1:
+                mid = mid+1
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high = high - 1
             
 
 
