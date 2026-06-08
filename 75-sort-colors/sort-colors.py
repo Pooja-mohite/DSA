3dbb1f041bc23d31e457e3417264b19ea22e184d
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-06-08 21:52:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (55.55%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,11 +4,27 @@ class Solution(object):
         # if condition is not given
         nums.sort()
         """
+        #  brute force
+        """
         n= len(nums)
         for i in range(n):
             for j in range(n-1):
                 if nums[j] > nums[j+1]:
                     nums[j], nums[j+1] = nums[j+1], nums[j]
+                    """
+        n = len(nums)            
+        left = 0
+        right = n-1
+        for i in range(n):
+            if nums[i] == 0:
+                nums[i], nums[left] = nums[left], nums[i]
+                left = left + 1
+        for i in range(n-1,-1,-1):
+            if nums[i] == 2:
+                nums[i], nums[right] = nums[right], nums[i]
+                right = right - 1
+            
+
 
        
         
