cb61644f1c3009a4335b527eaf3785cddbab4f25
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-06-11 12:19:18 +0530

    Time: 8 ms (69.04%) | Memory: 13.5 MB (57.64%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,42 +1,45 @@
 class Solution(object):
     def trap(self, height):
-        """
-        # brute force
-        total_water = 0
-        for i in range(len(height)):
-            left_max = 0
+        #brute force
+        '''
+        totalwater = 0
+        n = len(height)
+        for i in range(n):
+            leftmax = 0
             for j in range(i+1):
-                left_max = max(left_max, height[j])
-            
-            right_max = 0
-            for j in range(i, len(height)):
-                right_max = max(right_max, height[j])
-
-            water = min(left_max, right_max) - height[i]
-            total_water = total_water + water
-        return total_water
-        """
-
-        #two pointers
+                leftmax = max(leftmax, height[j])
+            rightmax = 0
+            for j in range(i, n):
+                rightmax = max(rightmax, height[j])
+            water = min(leftmax, rightmax)- height[i]
+            totalwater = totalwater + water
+        return totalwater
+        '''
+        # using two pointers
         left = 0
         right = len(height)-1
-        left_max = 0
-        right_max = 0
-        total_water = 0
+        leftmax =0
+        rightmax = 0
+        totalwater = 0
         while left < right:
             if height[left] < height[right]:
-                if height[left] >= left_max:
-                    left_max = height[left]
+                if height[left] > leftmax:
+                    leftmax = height[left]
                 else:
-                    total_water = total_water + left_max - height[left]
+                    totalwater = totalwater + leftmax - height[left]
                 left = left + 1
             else:
-                if height[right] >= right_max:
-                    right_max = height[right]
+                if height[right] > rightmax:
+                    rightmax = height[right]
                 else:
-                    total_water = total_water + right_max- height[right]
-                right = right - 1
-        return total_water
+                    totalwater = totalwater + rightmax - height[right]
+                right =right-1
+        return totalwater 
 
-        
-        
+
+
+
+
+
+
+       
