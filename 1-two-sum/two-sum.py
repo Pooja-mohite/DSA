e3a0c5118670b7f5b0789ec4673e05778574692e
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-06-24 22:26:39 +0530

    Time: 10 ms (44.90%) | Memory: 13.8 MB (16.52%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -6,7 +6,7 @@ class Solution(object):
                 if nums[i] + nums[j] == target:
                     return [i,j]
                     """
-
+        """
         hashmap = {}
         n = len(nums)
         for i in range(n):
@@ -14,6 +14,23 @@ class Solution(object):
             if required in hashmap:
                 return hashmap[required],i
             hashmap[nums[i]] = i
+            """
+        arr =[]
+        for i in range(len(nums)):
+            arr.append((nums[i], i))
+        arr.sort()
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            currentsum = arr[left][0] + arr[right][0]
+            if currentsum == target:
+                return [arr[left][1], arr[right][1]]
+
+            elif currentsum<target:
+                left = left +1
+            else:
+                right= right-1
+
 
 
         
