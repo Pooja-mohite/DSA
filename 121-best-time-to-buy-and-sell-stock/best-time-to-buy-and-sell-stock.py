cce7a68cc75985db1959b23be53d63935b2a926e
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-06-25 20:20:00 +0530

    Time: 33 ms (88.17%) | Memory: 19 MB (74.80%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -8,7 +8,7 @@ class Solution(object):
                 max_profit = max(max_profit, profit)
         return max_profit
         '''
-
+        """
         left = 0
         right = 1
         max_profit = 0
@@ -21,7 +21,17 @@ class Solution(object):
                 left = right
             right = right+1
         return max_profit
+        """
 
+        min_price = prices[0]
+        max_profit = 0
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            profit = price - min_price
+            if profit > max_profit:
+                max_profit = profit
+        return max_profit
 
         
        
