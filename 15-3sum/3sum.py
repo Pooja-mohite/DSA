6220ebd078f1432608af8c24e4fbaadd0de021b1
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-07-14 18:04:22 +0530

    Time: 1079 ms (20.96%) | Memory: 18.3 MB (79.31%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,40 +1,74 @@
 class Solution(object):
         def threeSum(self, nums):
-            '''
-            #Brute force
+            # BRUTE fORCE
+            """
             n = len(nums)
+            tsum = []
+            for i in range(n):
+                for j in range(i+1,n):
+                    for k in range(j+1,n):
+                        if nums[i]+ nums[j]+nums[k] == 0:
+                            summ = sorted([nums[i],nums[j],nums[k]])
+                            if summ not in tsum:
+                                tsum.append(summ)
+            return tsum 
+            """
+            """n= len(nums)
             tsum = set()
             for i in range(n):
-                for j in range(i+1, n):
-                    for k in range(j+1, n):
+                for j in range(i+1,n):
+                    for k in range(j+1,n):
                         if nums[i]+nums[j]+nums[k] == 0:
-                            result = tuple(sorted([nums[i],nums[j],nums[k]]))
-                            tsum.add(result)
-            return list(tsum)
-            '''
-            #optimized(two pointers)
-            n= len(nums)
+                            summ = tuple(sorted([nums[i], nums[j], nums[k]]))
+                            tsum.add(summ)
+            return list(tsum)"""
+
+            # hashset
+            """result = set()
+            n = len(nums)
+            for i in range(n):
+                hashmap = set()
+                for j in range(i+1,n):
+                    target = -(nums[i]+nums[j])
+                    if target in hashmap:
+                        tsum = [nums[i],nums[j], target]
+                        tsum.sort()
+                        result.add(tuple(tsum))    
+                    else:
+                        hashmap.add(nums[j])
+            return list(result)"""
+
+            #Two poinrtr
             nums.sort()
-            tsum = []
-            
+            result = []
+            n =len(nums)
             for i in range(n):
                 if i>0 and nums[i] == nums[i-1]:
                     continue
                 left = i+1
                 right = n-1
-                while left < right:
-                    if nums[i]+nums[left]+nums[right] ==0:
-                        tsum.append([nums[i],nums[left],nums[right]])
-                        left = left +1
-                        right = right - 1
-                        while left<right and nums[left] == nums[left-1]:
-                            left = left + 1
-                        while left< right and nums[right] == nums[right+1]:
-                            right = right-1
+                while left<right:
+                    if nums[i]+nums[left]+nums     [right] == 0:
+                        result.append([nums[i],nums[left],nums[right]])
+                        left = left+1
+                        right= right-1
+                        while left<n and nums      [left] == nums[left-1]:
+                            left = left+1
+                        while right>= 0 and nums[right]==nums[right+1]:
+                            right = right -1
                     elif nums[i]+nums[left]+nums[right] < 0:
-                        left = left +1
+                        left = left+1
                     else:
-                        right = right-1
-            return tsum
+                        right= right-1
+            return result
+
+
+
+
+
 
             
+            
+            
+           
+           
