2ca9f19a59d322e505a556a19a12c454690b8ff3
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-07-16 15:20:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (54.76%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,46 +1,23 @@
 class Solution(object):
     def sortColors(self, nums):
-        """
-        # if condition is not given
-        nums.sort()
-        """
-        #  brute force
-        """
-        n= len(nums)
-        for i in range(n):
-            for j in range(n-1):
-                if nums[j] > nums[j+1]:
-                    nums[j], nums[j+1] = nums[j+1], nums[j]
-                    """
-        # two pointers  
-        '''           
-        n = len(nums)            
-        left = 0
-        right = n-1
-        for i in range(n):
-            if nums[i] == 0:
-                nums[i], nums[left] = nums[left], nums[i]
-                left = left + 1
-        for i in range(n-1,-1,-1):
-            if nums[i] == 2:
-                nums[i], nums[right] = nums[right], nums[i]
-                right = right - 1
-                '''
-        # 3 pointers(dutch national flag)
         n = len(nums)
-        low = 0
-        mid = 0
-        high = n-1
-        while mid<= high:
-            if nums[mid] == 0:
-                nums[low], nums[mid] = nums[mid], nums[low]
-                low = low + 1
-                mid = mid+1
-            elif nums[mid] == 1:
-                mid = mid+1
+        zero = []
+        one = []
+        two = []
+        for i in range(n):
+            if nums[i]== 0:
+                zero.append(nums[i])
+            elif nums[i] == 1:
+                one.append(nums[i])
             else:
-                nums[mid], nums[high] = nums[high], nums[mid]
-                high = high - 1
+                two.append(nums[i])
+        del nums[:]
+        nums.extend(zero)
+        nums.extend(one)
+        nums.extend(two)
+        return nums
+            
+       
             
 
 
