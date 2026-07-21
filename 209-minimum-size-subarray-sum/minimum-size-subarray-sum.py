545d81f51b4b3c3809cadaf36e239be86fa2a16e
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-07-21 23:05:01 +0530

    Time: 23 ms (52.99%) | Memory: 19.3 MB (95.10%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,42 +1,45 @@
 class Solution(object):
     def minSubArrayLen(self, target, nums):
-        # brute force
-        """n = len(nums)
-        min_len = float("inf")
-        for i in range(n):
-            summ = 0
-            for j in range(i,n):
-                summ = summ + nums[j]
-                if summ >= target:
-                    leng = j -i +1
-                    if leng < min_len:
-                        min_len = leng
-                
-            if min_len == float("inf"):
-                return 0
-        
-        return min_len"""
 
-        left = 0
-        right = 0
+        """
         n = len(nums)
         min_len = float("inf")
-        summ = 0
-        while right<n:
-            summ = summ + nums[right]
-            while summ >= target:
-                leng = right - left + 1
+        for i in range(n):
+            suum = 0
+            for j in range(i,n):
+                suum = suum +nums[j]
+                if suum >= target:
+                    leng = j - i +1
+                    min_len = min(min_len, leng)
+            if min_len == float("inf"):
+                return 0
+        return min_len"""
+
+        l = 0
+        r = 0
+        suum = 0
+        n = len(nums)
+        min_len = float("inf")
+        while r < n:
+            suum = suum + nums[r]
+            while suum >= target:
+                leng = r- l+ 1
                 if leng < min_len:
                     min_len = leng
-                summ = summ - nums[left]
-                left = left +1
-            right = right + 1
-        if min_len == float("inf"):
+                suum= suum- nums[l]
+                l = l +1
+            r = r + 1       
+        if min_len == float("inf"):           
             return 0
         return min_len
 
-             
 
 
+
+
+
+
+        
+        
 
        
