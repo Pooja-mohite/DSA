76e4063f46592e1dc1dacd19fcc31b40cf0b2687
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-07-24 20:13:42 +0530

    Time: 19 ms (57.45%) | Memory: 12.4 MB (52.86%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -5,7 +5,7 @@ class Solution(object):
             return n
         return self.fib(n-1)+ self.fib(n-2)
         """
-
+        """
         self.dp=[-1]*(n+1)
         return self.find(n)
     def find(self,n):
@@ -14,7 +14,15 @@ class Solution(object):
         if n < 2:
             return n
         self.dp[n] = self.find(n-1)+self.find(n-2)
-        return self.dp[n]
+        return self.dp[n]"""
+
+        if n < 2:
+            return n
+        dp = [0] * (n+1)
+        dp[1] = 1
+        for i in range(2,n+1):
+            dp[i] = dp[i-1]+dp[i-2]
+        return dp[n]
         
 
         
