e16dbb4adf75e56e7c5dfefd1c56bb223e434a6f
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-08-02 22:09:45 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (18.34%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def isHappy(self, n):
+        # traverse all digit of the number
+        #take last didgit and square it
+        # add that square
+
+        summ = 0
+        hashset = set()
+        while n not in hashset:
+            if n == 1:
+                return True
+            else:
+                hashset.add(n)
+            summ = 0
+            while n > 0:
+                lastd = n% 10
+                square = lastd * lastd
+                summ = summ + square
+                n = n // 10
+            n = summ
+        return False
+        
+
+        
+        
