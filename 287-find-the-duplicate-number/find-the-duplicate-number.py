40de6425dadb392c301a520000276761d0d0ee2b
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-08-04 19:35:19 +0530

    Time: 35 ms (65.19%) | Memory: 21 MB (43.95%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,10 +1,26 @@
 class Solution(object):
     def findDuplicate(self, nums):
+        """
         n =len(nums)
         hashset = set()
         for i in range(n):
             if nums[i] in hashset:
                 return nums[i]
-            hashset.add(nums[i])
+            hashset.add(nums[i])"""
+
+
+        slow = nums[0]
+        fast = nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+   
+        return slow
        
        
