582e076351be8f14e08bf8f9989a99d93935f6a0
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-08-06 00:09:42 +0530

    Time: 7 ms (15.08%) | Memory: 12.5 MB (18.13%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,10 +1,11 @@
 class Solution(object):
+
     def isHappy(self, n):
         # traverse all digit of the number
         #take last didgit and square it
         # add that square
 
-        summ = 0
+        """summ = 0
         hashset = set()
         while n not in hashset:
             if n == 1:
@@ -18,7 +19,29 @@ class Solution(object):
                 summ = summ + square
                 n = n // 10
             n = summ
-        return False
+        return False"""
+
+        # take last digit and square it
+        # slow = n and fast = n
+        # slow 1 step and fast take 2 steps till fast not equal to 1
+
+        slow = n
+        fast = n
+        while fast!= 1:
+            slow = self.fun(slow)
+            fast = self.fun(fast)
+            fast = self.fun(fast)
+            if slow == fast and slow != 1:
+                return False
+        return True
+    def fun(self,n):
+        summ = 0
+        while n>0:
+            d = n % 10
+            n = n // 10
+            summ = summ + (d * d)
+        return summ
+
 
 
 
