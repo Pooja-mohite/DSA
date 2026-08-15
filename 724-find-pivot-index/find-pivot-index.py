ea5be7db70106c0f1e4228a31271b4757d45c4c7
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-08-15 19:58:51 +0530

    Time: 11 ms (31.04%) | Memory: 13.3 MB (57.64%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        # left side sum == right side sum retun idex else retun -1
+
+        """n = len(nums)
+        for i in range(n):
+            lsum = 0
+            rsum =0
+            for j in range(i):
+                lsum = lsum + nums[j]
+            for j in range(i+1, n):
+                rsum =  rsum + nums[j]
+            if lsum == rsum:
+                return i
+        return -1"""
+        # prefix
+        n = len(nums)
+        left = 0
+        ssum = sum(nums)
+        for i in range(n):
+            right = ssum - nums[i] - left
+            if left == right:
+                return i
+            left = left + nums[i]
+        return -1
+        
+                
+                
+
+        
+        
