c20119e2d3659da30cb23785e6d7cf884dda6ccf
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-09-19 20:58:54 +0530

    Time: 77 ms (5.31%) | Memory: 17.1 MB (9.86%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,47 +1,50 @@
 class Solution(object):
     def merge(self, intervals):
-        '''
-        #brute force
-        result = intervals[:]
-        while True:
-            n = len(result)
-            merged = False
-            for i in range(n):
-                for j in range(i+1,n):
-                    a = result[i]
-                    b = result[j]
-                    if a[1] >= b[0] and b[1] >= a[0]:
-                        newstart = min(a[0],b[0])
-                        newend= max(a[1], b[1])
+        # create one visisted list, check all intervals using for loop, if it is not visited then add it in to list, if it is visited then skip and move forward
+        # second for loop = compare each i with j intervals
+        #check overlap exits or not = overlap = i[end] >= j[start] and j[end] < i[start]
+        # merge overlapped intervals and add in list
+        
+        """n = len(intervals)
+        res = []
+        visited = [False]*n
+        for i in range(n):
+            if visited[i]:
+                continue
+            start = intervals[i][0]
+            end = intervals[i][1]
+            for j in range(i+1,n):
+                if visited[j]:
+                    continue
+                if not (intervals[j][1] < start or intervals[j][0] > end):
+                    start = min(start, intervals[j][0])
+                    end = max(end, intervals[j][1])
+                    visited[j] = True
+            res.append([start, end])
+        return res"""
 
-                        result.pop(j)
-                        result.pop(i)
-                        result.append([newstart, newend])
-                        merged = True
-                        break
-                if merged:
-                        break
-            if not merged:
-                    break
-        return result
-        '''
-
-        #optimized
-        intervals.sort(key=lambda x: x[0])
-        merged = []
-        for interval in intervals:
-            if len(merged) == 0:
-                merged.append(interval)
+        # first sort intervals, then check overlap(end < start)
+        n = len(intervals)
+        intervals.sort()
+        relist = []
+        start = intervals[0][0]
+        end = intervals[0][1]
+        for i in range(1,n):
+            s = intervals[i][0]
+            e = intervals[i][1]
+            if end >= s:
+                end = max(end,e)
             else:
-                prev_interval = merged[-1]
+                relist.append([start,end])
+                start = s
+                end = e
+        relist.append([start,end])
+        print(relist)
+        return relist
 
-                if interval[0] <= prev_interval[1]:
-                    prev_interval[0] = min(prev_interval[0], interval[0])
-                    prev_interval[1] = max(prev_interval[1], interval[1])
-                else:
-                    merged.append(interval)
-        return merged
 
+
+        
                 
     
         
