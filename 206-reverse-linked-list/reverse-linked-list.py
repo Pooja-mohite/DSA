f97170f7bcdf9f87d7a65a69b3ffc7ae20126457
Pooja mohite <144132430+Pooja-mohite@users.noreply.github.com>
2026-09-25 00:09:46 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (60.54%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -6,30 +6,33 @@
 class Solution(object):
     def reverseList(self, head):
         #Brute force
-        """
-        values = []
+        # take one empty list, and store the head in temp variable, then while temp != null, add data in empty list...then reverse that list and return head
+        """emplist = []
         temp = head
         while temp:
-            values.append(temp.val)
+            emplist.append(temp.val)
             temp = temp.next
-        values.reverse()
+        emplist.reverse()
         temp = head
         i = 0
         while temp:
-            temp.val = values[i]
+            temp.val = emplist[i]
             i = i+1
             temp = temp.next
-        return head
-        """
-        #Optimized
+        return head"""
+
+        # 3 pointers
+        # save next node in nextnode and then reverse the currentnext 
         prev = None
-        current = head
-        while current:
-            next_node = current.next
-            current.next = prev
-            prev = current
-            current = next_node
+        curr = head
+        while curr:
+            nextnode = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextnode
         return prev
+
+
 
      
         
